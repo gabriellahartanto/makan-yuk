add33a15daa9d3306a18d697dd1e8c55f07c93fb
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-08-26 21:06:00 +0700

    association meals

--- models/meal.js
+++ models/meal.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Meal.belongsToMany(models.Student, { through: models.StudentMeal })
     }
   };
   Meal.init({
