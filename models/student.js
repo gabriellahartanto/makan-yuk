214d529335894a73d33778fb3503f8fe421d256f
Bramah Ing Sona <ingsona06@gmail.com>
2020-08-28 00:51:27 +0700

    topup-blom-kelar

--- models/student.js
+++ models/student.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    
+    fullname() {
+      return `${this.first_name} ${this.last_name}`;
+    }
+
     static associate(models) {
       // define association here
       Student.belongsToMany(models.Meal, { through: models.StudentMeal, foreignKey: 'id_student' })
